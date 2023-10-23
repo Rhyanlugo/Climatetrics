3fb738cbc1af370bce8bc3ce428bd2669ab097cb
Rhyan Lugo Crespo <rhyanlugocrespo@gmail.com>
2023-10-23 01:02:52 -0400

    Added navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <div>
-      <h1 className=" text-center text-xl font-semibold text-yellow-500">
-        Hello World!
-      </h1>
+      <Navbar />
     </div>
   );
 }
