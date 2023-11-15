f8ae49eb1de4e11bcf2a0f47a26684505063aee1
Rhyan Lugo Crespo <rhyanlugocrespo@gmail.com>
2023-11-15 03:28:33 -0500

    changes to background and homepage

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -4,25 +4,27 @@ import styles from './Homepage.module.css';
 
 export default function Homepage() {
   return (
-    <main className={styles.homepage}>
-      <PageNav />
+    <div className="">
+      <main className={styles.homepage}>
+        <PageNav />
 
-      <section>
-        <h1>
-          Climate affects us all.
-          <br />
-          Climatetrics helps you visualize it.
-        </h1>
-        <h2>
-          We recognize that climate and weather play an integral role in all
-          aspects of our lives, not just on an individual basis but on a global
-          scale.
-        </h2>
+        <section>
+          <h1>
+            Climate affects us all.
+            <br />
+            Climatetrics helps you visualize it.
+          </h1>
+          <h2>
+            We recognize that climate and weather play an integral role in all
+            aspects of our lives, not just on an individual basis but on a
+            global scale.
+          </h2>
 
-        <Link to="/visualization" className="cta">
-          Start visualizing now
-        </Link>
-      </section>
-    </main>
+          <Link to="/visualization" className="cta hover:bg-blue-200">
+            Start visualizing now
+          </Link>
+        </section>
+      </main>
+    </div>
   );
 }
